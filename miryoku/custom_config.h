b50ac542188cc67d9a8d973a9109bc326bfc1f9b
Elisiano Petrini <elisiano@gmail.com>
2023-04-16 15:32:00 -0400

    [elisiano] commented customized MIRYOKU_LAYER_BUTTON

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -2,8 +2,8 @@
 // https://github.com/manna-harbour/miryoku
 
 
-#define MIRYOKU_LAYER_BUTTON \
-U_UND,             LS(U_CUT),         LS(U_CPY),         LS(U_PST),         U_RDO,             U_RDO,             LS(U_PST),         LS(U_CPY),         LS(U_CUT),         U_UND,             \
-&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NU,              U_NU,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
-U_UND,             U_CUT,             U_CPY,             U_PST,             U_RDO,             U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
-U_NP,              U_NP,              U_BTN3,            U_BTN1,            U_BTN2,            U_BTN2,            U_BTN1,            U_BTN3,            U_NP,              U_NP
+// #define MIRYOKU_LAYER_BUTTON \
+// U_UND,             LS(U_CUT),         LS(U_CPY),         LS(U_PST),         U_RDO,             U_RDO,             LS(U_PST),         LS(U_CPY),         LS(U_CUT),         U_UND,             \
+// &kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NU,              U_NU,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+// U_UND,             U_CUT,             U_CPY,             U_PST,             U_RDO,             U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
+// U_NP,              U_NP,              U_BTN3,            U_BTN1,            U_BTN2,            U_BTN2,            U_BTN1,            U_BTN3,            U_NP,              U_NP
