d629ebe6a1640b330560f6760ea5e93fd1498257
Elisiano Petrini <elisiano@gmail.com>
2023-04-18 18:15:06 -0400

    [elisiano] adding new line to custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -7,3 +7,4 @@ U_UND,             U_CUT,             &kp LS(LC(C)),     &kp LS(LC(V)),     U_RD
 &kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NU,              U_NU,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
 U_UND,             U_CUT,             U_CPY,             U_PST,             U_RDO,             U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
 U_NP,              U_NP,              U_BTN3,            U_BTN1,            U_BTN2,            U_BTN2,            U_BTN1,            U_BTN3,            U_NP,              U_NP
+
